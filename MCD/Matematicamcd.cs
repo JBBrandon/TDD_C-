ca62a6b7bd730b7a8fa75e0ccdbb47572c4e81b7
JBBrandon <saitoichijha@gmail.com>
2024-05-08 22:02:16 -0500

    primer commit

---
+++ MCD/Matematicamcd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD
+{
+    public class MatematicasMCD
+    {
+        public static void Main(string[] args)
+        {
+            // Aquí puedes llamar a tus métodos o hacer otras operaciones si lo deseas
+            int resultado = MCDcuatroNumeros(10, 15, 20, 25);
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+
+        public static int MCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static int MCDcuatroNumeros(int a, int b, int c, int d)
+        {
+            int mcdAB = MCD(a, b);
+            int mcdCD = MCD(c, d);
+            return MCD(mcdAB, mcdCD);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using MCD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_mcd_cuatro_numeros_basico()
+        {
+            Assert.AreEqual(5, MatematicasMCD.MCDcuatroNumeros(10, 15, 20, 25));
+            Assert.AreEqual(10, MatematicasMCD.MCDcuatroNumeros(10, 20, 30, 40));
+            Assert.AreEqual(5, MatematicasMCD.MCDcuatroNumeros(10, 15, 20, 25));
+        }
+        [TestMethod]
+        public void Test_mcd_cuatro_numeros_negativos()
+        {
+            Assert.AreEqual(5, Math.Abs(MatematicasMCD.MCDcuatroNumeros(10, -15, -20, -25)));
+            Assert.AreEqual(10, Math.Abs(MatematicasMCD.MCDcuatroNumeros(10, -20, -30, -40)));
+
+        }
+    }
+}
